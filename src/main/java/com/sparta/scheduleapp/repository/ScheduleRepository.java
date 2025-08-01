c91a0824367c572c0b2da7abb5bea5b054f3d461
jihoLee0818 <leejiho0114@gmail.com>
2025-08-01 19:41:45 +0900

    Feat: ScheduleRepository에 메소드 추가

--- src/main/java/com/sparta/scheduleapp/repository/ScheduleRepository.java
+++ src/main/java/com/sparta/scheduleapp/repository/ScheduleRepository.java
@@ -3,5 +3,9 @@ package com.sparta.scheduleapp.repository;
 import com.sparta.scheduleapp.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    List<Schedule> findAllByManagerOrderByModifiedAtDesc(String manager);
+    List<Schedule> findAllByOrderByModifiedAtDesc();
 }
